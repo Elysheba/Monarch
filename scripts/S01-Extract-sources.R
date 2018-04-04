a07df634dd8df57c4d6d4f36dace0de5371a9205
lfrancois <liesbeth.francois@ucb.com>
2018-04-04 17:33:51 +0200

    First commitment Monarch extract and process Rscripts

---
+++ scripts/S01-Extract-sources.R
@@ -0,0 +1,23 @@
+setwd("~/Shared/Data-Science/Data-Source-Model-Repository/Monarch/scripts/")
+
+library(RJSONIO)
+source("../../00-Utils/downloadSourceFiles.R")
+
+desc <- readJSONStream("../DESCRIPTION.json")
+
+sourceFiles <- desc$"source files"
+urls <- unlist(lapply(
+   sourceFiles,
+   function(sf){
+      toRet <- sf$"URL template"
+      names(toRet) <- sf$"name"
+      return(toRet)
+   }
+))
+urls["mondo.owl"] <- sprintf(
+   urls["mondo.owl"],
+   format(Sys.Date(), "%Y-%m")
+)
+srcDir <- "../sources"
+
+downloadSourceFiles(urls, srcDir)
